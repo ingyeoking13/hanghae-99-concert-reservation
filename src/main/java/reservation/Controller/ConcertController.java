463557c22d138cab34b90e393b813808bb20954e
Jeong Yo Han <ingyeoking13@gmail.com>
2024-04-10 22:00:56 +0900

    feature: controller 추가

--- src/main/java/reservation/Controller/ConcertController.java
+++ src/main/java/reservation/Controller/ConcertController.java
@@ -1,26 +1,31 @@
 package reservation.Controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import reservation.Controller.DTO.BaseResponse;
 import reservation.Controller.DTO.Show;
 import reservation.Service.ConcertShowService;
+import reservation.Service.DTO.ConcertShow;
 import reservation.Service.Exception.TokenUnavailableException;
 import reservation.Service.Exception.WaitingException;
 import reservation.Service.TicketService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
 public class ConcertController {
     private TicketService ticketService;
     private ConcertShowService concertShowService;
-    @GetMapping("/concerts")
-    public BaseResponse<List<Show>> getConcerts(@RequestHeader("token_id") String tokenId) {
-        List<Show> result = new ArrayList<>();
+    @GetMapping("/concerts/{concert_id}")
+    public BaseResponse<List<Show>> getConcerts(
+            @RequestHeader("token_id") String tokenId,
+            @PathParam("concert_id") Long concertId) {
+        List<Show> result = new LinkedList<>();
         try {
             ticketService.poolingWaitingQueue("concert" , tokenId);
         }
@@ -31,10 +36,15 @@ public class ConcertController {
             return new BaseResponse<>(404, "아직 대기열에 있습니다. " + e.getWaitingNumber() , result);
         }
 
-        Show testShow = Show.builder()
-                .name("test")
-                .dateTime(LocalDateTime.MAX).point(1000).build();
-        result.add(testShow);
+        List<ConcertShow> concertShows = concertShowService.getAvailableConcertShow(concertId);
+        concertShows.stream().forEach(
+                (concertShow) -> result.add(
+                        Show.builder()
+                                .name(concertShow.getName())
+                                .dateTime(concertShow.getDatetime())
+                                .build()
+                )
+        );
 
         return new BaseResponse<>(
                 200,
